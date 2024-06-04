747ff52fc22798540a4b5f85f83cac0060439380
Gonzalo Arroyo <87682632+Gonzalo1013@users.noreply.github.com>
2024-06-04 19:31:50 -0300

    url of images

--- src/figma/components/frame 8/Frame8.jsx
+++ src/figma/components/frame 8/Frame8.jsx
@@ -4,7 +4,7 @@ import './frame8.scss'
 export const Frame8 = () => {
   return (
     <div className='f8'>
-      <span className='h1_f8'>Wait ! your order in progress.</span>
+      <div className='h1_f8'>Wait ! your order in progress.</div>
       <p className='p_f8'>Lorem ipsum dolor sit, amet consectetur adipisicing</p>
     </div>
   )
